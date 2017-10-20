98b3d779b751e29c63cf8743893a8e6fe22d9e8a
Rita Azar <rita.azar0@gmail.com>
2017-10-20 14:21:41 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Example from './Example';
+
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      clicked: false
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      clicked: true
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button type="button" onClick={this.handleClick}>Click me</button>
+        {this.state.clicked ? <NewComponent /> : null}
+        {this.state.clicked ? <Example /> : null}
+      </div>
+    );
+  }
+}
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+class NewComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <Welcome name="Rita" />
+        <Welcome name="You" />
+      </div>
+    );
+  }  
+}
+
+export default App;
